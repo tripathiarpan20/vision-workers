2191a0800b11fadca3dc1b3504dde6b48f47c4ab
Arpan Tripathi <42506819+tripathiarpan20@users.noreply.github.com>
2025-02-06 22:06:01 +0000

    Stricter image checking penalty (#63)

--- validator_orchestrator/app/checking/functions/image_based.py
+++ validator_orchestrator/app/checking/functions/image_based.py
@@ -34,6 +34,10 @@ async def _get_image_similarity(
 
     probability_same_image_xg = images_are_same_classifier.predict_proba([hash_distances])[0][1]
 
+    #If the miner's image is blatantly different from the expected image, assign score of -10 
+    if clip_embedding_similiarity < 0.6:
+        return -10
+
     # MODEL has a very low threshold
     score = float(probability_same_image_xg**0.5) * 0.4 + (clip_embedding_similiarity**2) * 0.6
     if score > 0.95:
